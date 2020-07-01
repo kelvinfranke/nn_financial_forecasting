486f992f6bee26d03b96881afeca7a591c56b840
Kelvin Franke <kelvinfranke@gmail.com>
2020-07-01 11:37:59 +0200

    working on MICRO dataset

--- Code/ReadingInput.py
+++ Code/ReadingInput.py
@@ -5,87 +5,6 @@ import random
 
 
 class ReadingInput:
-<<<<<<< HEAD
-  # Normalizing function; values between 0 and 1.
-  def normalize(self, x, min, max):
-      return (x - min) / (max - min)
-
-  # Function to create some random noise between start 0 and the 1 - mean to not get a value bigger than 1
-  # Maybe changes this: - probability curve of the slope of the series. Uit deze distributie halen we waardes
-  def random_noise(self):
-      noise = random.uniform(-0.10, 0.10)
-      return noise
-
-  def NaN_replacements(self, last_value, mean_slope):
-      trend = random.uniform(-2,2)
-      replacement = last_value + trend*mean_slope + self.random_noise()
-      return replacement
-
-  def process_data(self):
-      # Reading file
-      df = pd.read_csv('M3C.csv')
-
-      # Only data of Category FINANCE; indexes: 331 - 388
-      finance_df = df.loc[df['Category'] == "FINANCE     "]
-
-      # Subsetting only the ti  print(np.shape(final_df))meseries; dropping every other column
-      finance_df = finance_df.iloc[:, list(range(6, 53))]
-
-      final_df = np.zeros([59,47])
-      timeseries = 0
-      # Iterating through every row, applying the normalizing function
-      for index, row in finance_df.iterrows():
-          max_value = max(row)
-          min_value = min(row)
-          timeseries += 1
-          final_series = row
-          final_series = final_series.to_numpy()
-          # NaN vervangen met de vorige waarde + mean_Slope + random_noise
-          if row.hasnans:
-              number_of_NaNs = row.isna().sum()
-              # Index van de eerste NaN (column)
-              column_last_value = len(row) - number_of_NaNs
-              last_value = row.at[str(column_last_value)]
-
-              new_row = row.dropna()
-
-              filling_values = np.array([])
-              # Mean slope of the series
-              mean_slope = new_row.diff().mean()
-              while number_of_NaNs > 0:
-                  replacement = self.NaN_replacements(last_value,mean_slope)
-                  filling_values = np.append(filling_values,replacement)
-                  number_of_NaNs -= 1
-                  last_value = replacement
-
-              new_row = new_row.to_numpy()
-
-              final_series = np.concatenate((new_row,filling_values))
-          # print(final_series)
-
-          # Normalizing the row
-          normalized_row = self.normalize(final_series,min(final_series),max(final_series))
-          # print(normalized_row)
-
-          # Adding the new normalized, filled row to the final dataframe (PANDAS)
-          #final_df = final_df.append(normalized_row, ignore_index=True)
-
-          # Adding the new normalized, filled row to the final dataframe (NUMPY)
-          final_df[timeseries,:] = normalized_row
-          # self.plot(final_df, timeseries)
-          # print(len(final_df))
-          # print(np.shape(final_df))
-      return final_df
-
-
-  def plot(self, final_df, timeseries):
-    # Plotting one normalized row; just to visualize the data
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.plot(np.linspace(0, len(final_df[timeseries,:]), len(final_df[timeseries,:])), final_df[timeseries,:])
-    plt.show()
-      
-=======
     # Normalizing function; values between 0 and 1.
     def normalize(self, x, min, max):
         return (x - min) / (max - min)
@@ -106,12 +25,15 @@ class ReadingInput:
         df = pd.read_csv('M3C.csv')
 
         # Only data of Category FINANCE; indexes: 331 - 388
-        finance_df = df.loc[df['Category'] == "FINANCE     "]
+        # finance_df = df.loc[df['Category'] == "FINANCE     "]
+        finance_df = df.loc[df['Category'] == "MICRO       "]
 
         # Subsetting only the ti  print(np.shape(final_df))meseries; dropping every other column
-        finance_df = finance_df.iloc[:, list(range(6, 53))]
+        # finance_df = finance_df.iloc[:, list(range(6, 53))]
+        finance_df = finance_df.iloc[:, list(range(6, 27))]
 
-        final_df = np.zeros([59, 46])
+        # final_df = np.zeros([59, 46])
+        final_df = np.zeros([146, 20])
         timeseries = 0
         # Iterating through every row, applying the normalizing function
         for index, row in finance_df.iterrows():
@@ -149,7 +71,7 @@ class ReadingInput:
             # Adding the new normalized, filled row to the final dataframe (NUMPY)
             final_df[timeseries, :] = detrended_row
             self.plot(final_df, timeseries)
-            print(np.shape(final_df))
+            return(final_df)
 
     def plot(self, final_df, timeseries):
         # Plotting one normalized row; just to visualize the data
@@ -158,7 +80,6 @@ class ReadingInput:
         ax.plot(np.linspace(0, len(final_df[timeseries, :]), len(final_df[timeseries, :])), final_df[timeseries, :])
         plt.show()
 
->>>>>>> b7a59501f279b10dbbc2333fd09ead92cff4cce7
 
 if __name__ == "__main__":
     reading_input = ReadingInput()
