91130bc2afd526b41d1a47b6210bbd2d133f32a5
Matthiasd23 <47788983+Matthiasd23@users.noreply.github.com>
2020-07-02 12:35:58 +0200

    Update ReadingInput.py

--- Code/ReadingInput.py
+++ Code/ReadingInput.py
@@ -26,6 +26,17 @@ class ReadingInput:
         if replacement < -1:
             return -1
         return replacement
+    
+    # Exponential moving averager
+    def averager(self, row):
+        days = 14
+        smoothing = 2.1
+        averaged_row = np.array([])
+        new_value = 0
+        for value in row:
+            new_value = (value * (smoothing/(1+days))) + new_value*(1-(smoothing/(1+days)))
+            averaged_row = np.append(averaged_row, [new_value])
+        return averaged_row
 
     def process_data(self):
         # Reading file
@@ -46,38 +57,21 @@ class ReadingInput:
         for index, row in finance_df.iterrows():
             timeseries += 1
             final_series = row
+            # Length = 20
             final_series = final_series.to_numpy()
-            # NaN vervangen met de vorige waarde + mean_Slope + random_noise
-            if row.hasnans:
-                number_of_NaNs = row.isna().sum()
-                # Index van de eerste NaN (column)
-                column_last_value = len(row) - number_of_NaNs
-                last_value = row.at[str(column_last_value)]
-
-                new_row = row.dropna()
-
-                filling_values = np.array([])
-                # Mean slope of the series
-                mean_slope = new_row.diff().mean()
-                while number_of_NaNs > 0:
-                    replacement = self.NaN_replacements(last_value, mean_slope)
-                    filling_values = np.append(filling_values, replacement)
-                    number_of_NaNs -= 1
-                    last_value = replacement
-
-                new_row = new_row.to_numpy()
-
-                final_series = np.concatenate((new_row, filling_values))
-
-            # Normalizing the row
-            normalized_row = self.normalize(final_series, min(final_series), max(final_series))
-
             # Detrending
-            detrended_row = np.diff(normalized_row)
+            detrended_row = np.diff(final_series)
+            # Length = 19
 
-            # Adding the new normalized, filled row to the final dataframe (NUMPY)
-            final_df[timeseries, :] = detrended_row
-            # self.plot(final_df, timeseries)
+            # Averaging
+            averaged_row = self.averager(detrended_row)
+            # Length = 19
+
+            normalized_row = self.normalize(detrended_row, min(detrended_row), max(detrended_row))
+            normalized_averaged_row = self.normalize(averaged_row, min(averaged_row), max(averaged_row))
+
+            final_df[timeseries, :] = normalized_row
+            #self.plot(final_df, timeseries)
         return(final_df)
 
     def plot(self, final_df, timeseries):
