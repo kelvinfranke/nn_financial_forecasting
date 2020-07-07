9f3cfd40be5f0e898d7231f3831278d41a75cd27
Kelvin Franke <kelvinfranke@gmail.com>
2020-07-07 23:20:11 +0200

    final code

--- Code/network2.py
+++ Code/network2.py
@@ -33,7 +33,7 @@ from keras import backend as K
 import sys
 
 reading_input = ReadingInput()
-# Ik heb de functie uitgebreid zodat ie de max en de min van elke array onthoudt
+
 data, max_arr, min_arr = reading_input.process_data()
 
 X, y = list(), list()
@@ -41,8 +41,6 @@ for j in range(0, 146):
   sequence = data[j,:]
   X.append(sequence[:8])
   y.append(sequence[9:15])
-
-# X, y, X_cross_val, y_cross_val = train_test_split(X, y, test_size = 0.33, random_state = 42)
 
 X_val, y_val = list(), list()
 for i in range (0, 146):
@@ -59,7 +57,8 @@ y_val = np.array(y_val)
 random_indices = np.arange(0, 143, 1).tolist()
 np.random.shuffle(random_indices)
 
-# voert k-fold cross validation uit
+
+# executes k-fold cross-validation
 def kfold_procedure(X, y, model):
   k_fold = KFold(n_splits = 12)
   total_test = []
@@ -68,7 +67,6 @@ def kfold_procedure(X, y, model):
   top_model = 0
   avg_acc = 0
   for train_indices, test_indices in k_fold.split(X):
-    # print(train_indices)
     X_matrix = []
     y_matrix = []
     for i in train_indices:
@@ -77,8 +75,6 @@ def kfold_procedure(X, y, model):
       y_matrix.append(y[i])
     X_matrix = np.array(X_matrix)
     y_matrix = np.array(y_matrix)
-    # print(np.shape(X_matrix))
-    # print(np.shape(y_matrix))
     history = model.fit(X_matrix, y_matrix, epochs=100, verbose=0, shuffle=False)
     X_test = []
     y_test = []
@@ -96,17 +92,15 @@ def kfold_procedure(X, y, model):
     avg_acc += results[1]
     # model.save('model' + str(save))
     save += 1
-    # print(train_indices, test_indices)
-  # print(total_test)
   print("Best model index: " + str(top_model))
   return avg_acc/12
 
 
-# Dit stukkie veranderd de hoeveelheid neurons in het model en het gebruikt die kfold_procedure functie om de avg accuracy te berekenen
+# Increases the amount of neurons in the hidden layer of the model until the best model is found
 def model_finder():
   acc = -99999
   flag = 0
-  for j in range(8, 20):
+  for j in range(1, 20):
     model = Sequential()
     model.add(Dense(j, activation='relu', kernel_regularizer=regularizers.l2(0.01), input_dim=8))
     model.add(Dense(6))
@@ -134,9 +128,9 @@ def model_finder():
   print("bigger is better")
   return model
 
-model = tf.keras.models.load_model('model')
-
-# uncomment dit als je nieuwe model wil fitten (en de oude overwriten)
+model = tf.keras.models.load_model('model2')
+model.summary()
+# uncomment this if you want to fit a new model
 # model = model_finder()
 # model.evaluate(X_val, y_val, verbose=1)
 # model.save('model')
@@ -145,25 +139,8 @@ model = tf.keras.models.load_model('model')
 yhat = model.predict(X_val, verbose=1)
 yhat = np.array(yhat)
 
-# Hier draai ik het normalizeren terug
+# This reverts the normalizing
 for i in range(np.shape(yhat)[0]):
   yhat[i,:] = reading_input.reverse_normalize(yhat[i,:], min_arr[i], max_arr[i])
 
 print(yhat)
-# print("test loss, test acc:", results)
-# plt.figure() 
-# plt.plot(history.history['loss']) 
-# plt.plot(history.history['val_loss']) 
-# plt.title('model loss') 
-# plt.ylabel('loss') 
-# plt.xlabel('epoch') 
-# plt.legend(['train', 'test'], loc='best') 
-# plt.show() 
-# plt.figure() 
-# plt.plot(history.history['acc']) 
-# plt.plot(history.history['val_acc']) 
-# plt.title('model accuracy') 
-# plt.ylabel('acc') 
-# plt.xlabel('epoch') 
-# plt.legend(['train', 'test'], loc='best') 
-# plt.show()
